793b52773215726c0bfe9bae6cbe99fe0462e6e6
sagar <sagarsaha3d95@gmail.com>
2021-06-26 17:10:54 +0530

    the second stage

--- todoList/base/views.py
+++ todoList/base/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
+from .models import Task
 
 # Create your views here.
-def base(request):
-    return HttpResponse('hello')
+# def base(request):
+#     return HttpResponse('hello')
+
+class TaskViews(ListView):
+    model = Task
+    context_object_name = 'all_tasks'
+    template_name = 'all-tasks.html'

38a8e8a5636690e88de24ca719c4073e30568d37
sagar <sagarsaha3d95@gmail.com>
2021-06-27 17:58:09 +0530

    all CRAD funtionality done

--- todoList/base/urls.py
+++ todoList/base/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.TaskViews.as_view(),name='all-task'),
+    path('',views.TaskViews.as_view(), name='all-task'),
+    path('task/<int:pk>/',views.TaskDescription.as_view(), name = 'task-description'),
+    path('task-create/',views.TaskCreate.as_view(), name = 'task-create'),
+    path('taskUpdate/<int:pk>/',views.TaskUpdate.as_view(), name= 'task-update'),
+    path('taskDelete/<int:pk>/',views.TaskDelete.as_view(), name= 'task-delete'),
+    
 ]
--- todoList/base/views.py
+++ todoList/base/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
 from .models import Task
 
 # Create your views here.
@@ -10,3 +13,28 @@ class TaskViews(ListView):
     model = Task
     context_object_name = 'all_tasks'
     template_name = 'all-tasks.html'
+
+class TaskDescription(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    context_object_name = 'form'
+    template_name = 'task-create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('all-task')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    context_object_name = 'form'
+    template_name = 'task-create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('all-task')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'confirmation.html'
+    success_url = reverse_lazy('all-task') 
